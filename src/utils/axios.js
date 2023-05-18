4cffcb38fce6c47ced905d18617fbc2147806f18
luka_CB <lukaaslamazashvili20@gmail.com>
2023-05-18 15:42:43 +0400

    Fixed some issues

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const url =
   import.meta.env.VITE_APP_ENV === "development"
     ? "http://localhost:5000"
-    : "https://techstore-server-production.up.railway.app";
+    : "https://techstore-api-3jmr.onrender.com";
 
 export default axios.create({
   baseURL: url,
